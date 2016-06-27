d8eed60757d361d584c7e9e8249bac5aff8c56c0
root <root@raspberrypi>
2016-06-27 07:41:29 +0000

    Update

--- im2cor.py
+++ im2cor.py
@@ -9,18 +9,22 @@ import math
 
 # Load an example image from ISS in grayscale 
 # http://spaceflight.nasa.gov/gallery/images/station/crew-6/hires/iss006e40544.jpg
-i = cv2.imread('iss006e40544.jpg',0)
+
+filename='iss006e40544'
+#filename='starfield'
+
+i = cv2.imread('images/'+filename+'.jpg',0)
 
 img = i[1:1200, 1:2000] # Do some crop under to remove the iss image code
 
 median1 = cv2.medianBlur(img,5)
-cv2.imwrite('iss006e40544_median1.png',median1)
+cv2.imwrite('images/'+filename+'_median1.png',median1)
 
 median2 = cv2.medianBlur(median1,5)
-cv2.imwrite('iss006e40544_median2.png',median2)
+cv2.imwrite('images/'+filename+'_median2.png',median2)
 
 ret,thresh1 = cv2.threshold(median2,110,255,cv2.THRESH_BINARY)
-cv2.imwrite('iss006e40544_otsu.png',thresh1)
+cv2.imwrite('images/'+filename+'_otsu.png',thresh1)
 
 detector = cv2.SimpleBlobDetector()
 keypoints = detector.detect(255-thresh1)
@@ -31,7 +35,7 @@ for s,kp in enumerate(keypoints):
 	#print (x, y)
 	cv2.circle(img,(int(x),int(y)),20 , (255,0,255),1 )
 	cv2.putText(img,str(s),(int(x)-22,int(y)-22), font, 0.6,(255,255,255))
-cv2.imwrite('iss006e40544_circles.png',img)
+cv2.imwrite('images/'+filename+'_circles.png',img)
 
 # compute angular distances 
 # (for geometrical corrected image)
