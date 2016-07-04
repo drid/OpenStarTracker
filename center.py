5ece81f93d16aff14a6e57e0f27df50dd732f1e4
jger <j.gerardis@gmail.com>
2016-07-04 11:53:59 +0000

    Commends changed

--- center.py
+++ center.py
@@ -1,28 +1,27 @@
 # This python program based on a BASIC program Written by Jordan d. Marche
 # and explained by him in Sky & Telescope for July, 1990, page 71.
+# Source: http://www.skyandtelescope.com/wp-content/uploads/marche.bas
  
 import numpy as np
 import math
 
 dr = math.pi/180
-L = 4 # camera focal length
+L = 0.030 # camera focal length
 N = 4 # number of stars, must be >=4
 
 
 # Test Data set for Major Ursa const. stars: Dubhe, Merak, Phad, Alkaid
 
-a = [ 11.062155*15*dr, 	11.030677*15*dr, 	11.897168*15*dr, 	13.792354*15*dr ]
-d = [ 61.751033*dr, 	56.382427*dr, 		53.69476*dr, 		49.313265*dr ]
+a = [ 11.062155*15*dr, 	11.030677*15*dr, 	11.897168*15*dr, 	13.792354*15*dr ] # converted to radians
+d = [ 61.751033*dr, 	56.382427*dr, 		53.69476*dr, 		49.313265*dr ]    # converted to radians
 x = [ 574.518737793, 	458.241729736, 		713.016113281, 		1445.30236816 ]
 y = [ 968.822937012, 	769.926208496, 		546.142456055, 		302.151184082 ]
 x1=[]
 y1=[]
 
-#xt = 1575/2 # Target (center of the image)
-#yt = 945/2
-xt = 688
-yt = 514
-
+# Target (center of the image)
+xt = 1999/2 
+yt = 1199/2
 
 a0 = np.mean(a) # R.a. of plate center approx. 
 d0 = np.mean(d)	# dec. of plate center approx.
